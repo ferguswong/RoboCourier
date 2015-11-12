bcb65ee3c4c708a90da67414e518876903c8a1b4
David Zong <davidzng@uw.edu>
2015-11-12 16:10:07 -0600

    added more sounds

--- coinsound/coinsound.ino
+++ coinsound/coinsound.ino
@@ -103,4 +103,34 @@ void loop() {
   tone(8,NOTE_E6,850);
   delay(800);
   noTone(8);
+  
+    delay(2000);  // pause 2 seconds
+
+  // Play 1-up sound
+  tone(8,NOTE_E6,125);
+  delay(130);
+  tone(8,NOTE_G6,125);
+  delay(130);
+  tone(8,NOTE_E7,125);
+  delay(130);
+  tone(8,NOTE_C7,125);
+  delay(130);
+  tone(8,NOTE_D7,125);
+  delay(130);
+  tone(8,NOTE_G7,125);
+  delay(125);
+  noTone(8);
+
+  delay(2000);  // pause 2 seconds
+
+  // Play Fireball sound
+  tone(8,NOTE_G4,35);
+  delay(35);
+  tone(8,NOTE_G5,35);
+  delay(35);
+  tone(8,NOTE_G6,35);
+  delay(35);
+  noTone(8);
+  
+  delay(2000);  // pause 2 seconds
 }
