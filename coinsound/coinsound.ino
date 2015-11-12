2525e19a4b3c3e5abb6b8f1ecdb31f9579bd2bd4
David Zong <davidzong1@gmail.com>
2015-11-12 16:15:43 -0600

    moved the coin sound function to work on pin 16

--- coinsound/coinsound.ino
+++ coinsound/coinsound.ino
@@ -98,39 +98,39 @@ void setup() {
 }
 
 void loop() {
-  tone(8,NOTE_B5,100);
+  tone(16,NOTE_B5,100);
   delay(100);
-  tone(8,NOTE_E6,850);
+  tone(16,NOTE_E6,850);
   delay(800);
-  noTone(8);
+  noTone(16);
   
-    delay(2000);  // pause 2 seconds
-
-  // Play 1-up sound
-  tone(8,NOTE_E6,125);
-  delay(130);
-  tone(8,NOTE_G6,125);
-  delay(130);
-  tone(8,NOTE_E7,125);
-  delay(130);
-  tone(8,NOTE_C7,125);
-  delay(130);
-  tone(8,NOTE_D7,125);
-  delay(130);
-  tone(8,NOTE_G7,125);
-  delay(125);
-  noTone(8);
-
-  delay(2000);  // pause 2 seconds
-
-  // Play Fireball sound
-  tone(8,NOTE_G4,35);
-  delay(35);
-  tone(8,NOTE_G5,35);
-  delay(35);
-  tone(8,NOTE_G6,35);
-  delay(35);
-  noTone(8);
-  
-  delay(2000);  // pause 2 seconds
+//    delay(2000);  // pause 2 seconds
+//
+//  // Play 1-up sound
+//  tone(8,NOTE_E6,125);
+//  delay(130);
+//  tone(8,NOTE_G6,125);
+//  delay(130);
+//  tone(8,NOTE_E7,125);
+//  delay(130);
+//  tone(8,NOTE_C7,125);
+//  delay(130);
+//  tone(8,NOTE_D7,125);
+//  delay(130);
+//  tone(8,NOTE_G7,125);
+//  delay(125);
+//  noTone(8);
+//
+//  delay(2000);  // pause 2 seconds
+//
+//  // Play Fireball sound
+//  tone(8,NOTE_G4,35);
+//  delay(35);
+//  tone(8,NOTE_G5,35);
+//  delay(35);
+//  tone(8,NOTE_G6,35);
+//  delay(35);
+//  noTone(8);
+//  
+//  delay(2000);  // pause 2 seconds
 }
