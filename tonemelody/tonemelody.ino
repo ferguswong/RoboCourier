167242c2e6ef675448e9ac0de8e044ced931f6f9
David Zong <davidzong1@gmail.com>
2015-11-12 16:51:18 -0600

    update to melody to play around with what it plays

--- tonemelody/tonemelody.ino
+++ tonemelody/tonemelody.ino
@@ -19,12 +19,12 @@ This example code is in the public domain.
 
 // notes in the melody:
 int melody[] = {
-  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
+  NOTE_C4, NOTE_C4, NOTE_C4, NOTE_D4, NOTE_E4, 0
 };
 
 // note durations: 4 = quarter note, 8 = eighth note, etc.:
 int noteDurations[] = {
-  4, 8, 8, 4, 4, 4, 4, 4
+  4, 4, 4, 8, 8, 4
 };
 
 void setup() {
