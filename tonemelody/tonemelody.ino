525f0a2550fa37132fccd18494dd2a8e8a8bad12
David Zong <davidzong1@gmail.com>
2015-11-19 15:07:35 -0600

    added loser sound for time attack

--- tonemelody/tonemelody.ino
+++ tonemelody/tonemelody.ino
@@ -109,10 +109,37 @@ void readyStart() {
   }
 }
 
+void loser() {
+  // notes in the melody:
+  int melody[] = {
+  NOTE_C5, 0, NOTE_B4, 0, NOTE_AS4
+  };
+  // note durations: 4 = quarter note, 8 = eighth note, etc.:
+  int noteDurations[] = {
+    4, 2, 4, 2, 1
+  };
+  // iterate over the notes of the melody:
+  for (int thisNote = 0; thisNote < 5; thisNote++) {
+
+    // to calculate the note duration, take one second
+    // divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(pin, melody[thisNote], noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    noTone(pin);
+  }
+}
+
 void setup() {
-  readyStart();
+  loser();
 }
 
 void loop() {
-  timeUp();
+  //timeUp();
 }
