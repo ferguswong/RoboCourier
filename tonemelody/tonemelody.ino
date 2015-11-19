df89c75aa8cda9e6ec3fbe5ae79e61198724a41c
David Zong <davidzong1@gmail.com>
2015-11-19 14:51:02 -0600

    added time attack sounds

--- tonemelody/tonemelody.ino
+++ tonemelody/tonemelody.ino
@@ -55,10 +55,64 @@ void coinSound() {
   noTone(pin);
 }
 
+void timeUp() {
+    // notes in the melody:
+  int melody[] = {
+  NOTE_C2, 0, NOTE_C2, 0, NOTE_C2, 0
+  };
+  // note durations: 4 = quarter note, 8 = eighth note, etc.:
+  int noteDurations[] = {
+    16, 16, 16, 16, 16, 16
+  };
+  // iterate over the notes of the melody:
+  for (int thisNote = 0; thisNote < 6; thisNote++) {
+
+    // to calculate the note duration, take one second
+    // divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(pin, melody[thisNote], noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    noTone(pin);
+  }
+}
+
+void readyStart() {
+  // notes in the melody:
+  int melody[] = {
+  NOTE_C2, 0, NOTE_C2, 0, NOTE_C4
+  };
+  // note durations: 4 = quarter note, 8 = eighth note, etc.:
+  int noteDurations[] = {
+    4, 2, 4, 2, 1
+  };
+  // iterate over the notes of the melody:
+  for (int thisNote = 0; thisNote < 5; thisNote++) {
+
+    // to calculate the note duration, take one second
+    // divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(pin, melody[thisNote], noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    noTone(pin);
+  }
+}
+
 void setup() {
-  winSound();
+  readyStart();
 }
 
 void loop() {
-  //coinSound();
+  timeUp();
 }
